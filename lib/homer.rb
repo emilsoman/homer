c508b4a74e843bfa782021c149cbd8550d80c0a0
Emil S <emil.soman@gmail.com>
2012-09-22 01:47:37 +0530

    Specs for homer to initialize and add dotfiles

---
+++ lib/homer.rb
@@ -0,0 +1,33 @@
+require 'fileutils'
+
+class Homer
+
+  def self.init
+    Dir.mkdir(File.join(Dir.home, ".homer"))
+    File.new(dotfiles_path , "w")
+  rescue Exception => e
+    raise "~/.homer cannot be created : #{e.message}"
+  end
+
+  def self.dotfiles_path
+    return File.join(Dir.home, ".homer", "dotfiles")
+  end
+
+  def self.root_path
+    return File.join(Dir.home, ".homer")
+  end
+
+  def self.wipe
+    FileUtils.rm_rf(File.join(Dir.home, ".homer"))
+  end
+
+  def self.add(dotfile)
+    dotfile = File.expand_path(dotfile)
+    f = File.open(dotfiles_path, "a")
+    raise "#{dotfile} does not exist." unless File.exists?(dotfile)
+    f.puts dotfile
+    f.close
+  end
+
+end
+
--- spec/homer_spec.rb
+++ spec/homer_spec.rb
@@ -1,7 +1,53 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'spec_helper'
 
 describe "Homer" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  describe "initialize" do
+    before (:each) do
+      Homer.wipe
+      Homer.init
+    end
+    it "should create a .homer folder in home directory" do
+      Dir.exists?(ENV['HOME']+'/.homer').should be_true
+    end
+    it "should create an empty dotfiles file in ~/.homer" do
+      File.zero?(ENV['HOME']+'/.homer/dotfiles').should be_true
+    end
+  end
+
+  describe "dotfiles_path" do
+    it "should return the dotfiles path" do
+      Homer.dotfiles_path.should == File.join(Dir.home, ".homer", "dotfiles") 
+    end
+  end
+
+  describe "root_path" do
+    it "should return the ~/.homer path" do
+      Homer.root_path.should == File.join(Dir.home, ".homer") 
+    end
+  end
+
+  describe "adding a dotfile" do
+    context "when dotfile doesnt exist" do
+      it "should raise error when dotfile does not exist" do
+        expect do
+          Homer.add('~/.file_that_does_not_exist')
+        end.to raise_error("/home/emil/.file_that_does_not_exist does not exist.")
+        #File.read(Homer.dotfiles_path).should include(File.join(Dir.home, ".file_that_does_not_exist"))
+      end
+    end
+    context "when dotfile exists" do
+      it "should raise error when dotfile does not exist" do
+        File.new(File.join(Dir.home, ".file_that_exists"), "w")
+        File.new(File.join(Dir.home, ".another_file_that_exists"), "w")
+        expect do
+          Homer.add('~/.file_that_exists')
+          Homer.add('~/.another_file_that_exists')
+        end.not_to raise_error
+        File.read(Homer.dotfiles_path).should include(File.join(Dir.home, ".file_that_exists"))
+        File.read(Homer.dotfiles_path).should include(File.join(Dir.home, ".another_file_that_exists"))
+        File.delete(File.join(Dir.home, ".file_that_exists"))
+        File.delete(File.join(Dir.home, ".another_file_that_exists"))
+      end
+    end
   end
 end
